939b9fd31139db71fe98c09e01a37dd465220d63
njenkins2727 <njenkins2727@gmail.com>
2025-07-18 16:07:31 +0800

    using up bank api to understand api routes with python

--- app.py
+++ app.py
@@ -13,21 +13,20 @@ load_dotenv()
 api_key = os.getenv("UP_API_KEY")
 database_url = os.getenv("DATABASE_URL")
 
+
 @app.route("/")
-def get_api():
-    headers = {
-        "Authorization": f"Bearer {api_key}"
-    }
-    res = requests.get('https://api.up.com.au/api/v1/accounts', headers=headers)
-    response = res.json()
-    display_name = response['data'][1]['attributes']['displayName']
-    print (display_name)
-    return f"<h1>{display_name}</h1>"
+def get_api(): #def = defines function + instead of {} we use indentation and colons:
+    try: # try and expect NOT try catch 
+        headers = {
+            "Authorization": f"Bearer {api_key}" # f = string literal 
+        }
+        res = requests.get('https://api.up.com.au/api/v1/accounts', headers=headers)
+        response = res.json()
+        display_name = response['data'][2]['attributes']['displayName'] #retrieving specific data from response 
+        print (display_name)
+        return f"<h1>{display_name}</h1>"
+    except:
+        print('Error: failed to get display name')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
