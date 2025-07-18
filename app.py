1e041c1bfa96e754f625a9b9101143064bc0b309
njenkins2727 <njenkins2727@gmail.com>
2025-07-18 12:33:01 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+x = 'Nathan'
+y = True
+
+if y == True:
+    print('Hello World my name is,', x)
+else:
+    print('My name is no longer', x)
+
+data = {
+    1: 'Pizza',
+    2: 'Icecream',
+    3: 'Rice'
+}
+
+print('MMM... I love ', data[2])
