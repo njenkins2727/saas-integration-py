5247ccd8faa3d5fe6be95be7a2f7629c06d9cf39
njenkins2727 <njenkins2727@gmail.com>
2025-07-18 15:31:19 +0800

    using up bank api to understand api routes with python

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
-x = 'Nathan'
-y = True
+from flask import Flask
+import os
+import requests
 
-if y == True:
-    print('Hello World my name is,', x)
-else:
-    print('My name is no longer', x)
+app = Flask(__name__)
 
-data = {
-    1: 'Pizza',
-    2: 'Icecream',
-    3: 'Rice'
-}
+from dotenv import load_dotenv
 
-print('MMM... I love ', data[2])
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the variables using os.getenv()
+api_key = os.getenv("UP_API_KEY")
+database_url = os.getenv("DATABASE_URL")
+
+@app.route("/")
+def get_api():
+    headers = {
+        "Authorization": f"Bearer {api_key}"
+    }
+    res = requests.get('https://api.up.com.au/api/v1/accounts', headers=headers)
+    response = res.json()
+    display_name = response['data'][1]['attributes']['displayName']
+    print (display_name)
+    return f"<h1>{display_name}</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
